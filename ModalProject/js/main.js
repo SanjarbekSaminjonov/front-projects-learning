c47ddf8ce97e56e311f53e861a5dd2c2a8b35141
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2021-12-24 00:40:57 +0500

    added left right up down keydowns

--- ModalProject/js/main.js
+++ ModalProject/js/main.js
@@ -3,25 +3,53 @@ const showModal = document.getElementById('show-modal-button');
 const hideModal = document.getElementById('hide-modal-button');
 const overlay = document.getElementById('overlay');
 
-
-showModal.addEventListener('click', () => show());
-
-hideModal.addEventListener('click', () => hide());
-
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') {
-        hide();
-    }
-});
-
-overlay.addEventListener('click', () => hide());
+let topB = 50;
+let leftB = 50;
 
 function show() {
     modalBox.classList.remove('hidden');
     overlay.classList.remove('hidden');
+
+    showModal.classList.add('hidden');
 }
 
 function hide() {
     modalBox.classList.add('hidden');
     overlay.classList.add('hidden');
+
+    showModal.classList.remove('hidden');
 }
+
+showModal.addEventListener('click', () => show());
+
+hideModal.addEventListener('click', () => hide());
+
+overlay.addEventListener('click', () => hide());
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        hide();
+    }
+
+    console.log(e.key);
+
+    if (e.key === "ArrowUp" && topB > 30) {
+        topB -= 4;
+        modalBox.style.top = topB + "%";
+    }
+
+    if (e.key === "ArrowDown" && topB < 70) {
+        topB += 4;
+        modalBox.style.top = topB + "%";
+    }
+
+    if (e.key === "ArrowLeft" && leftB > 30) {
+        leftB -= 4;
+        modalBox.style.left = leftB + "%";
+    }
+
+    if (e.key === "ArrowRight" && leftB < 70) {
+        leftB += 4;
+        modalBox.style.left = leftB + "%";
+    }
+});
