13a0d2eda9640897922eff3559fe00ee3c5f730f
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2022-01-01 21:58:24 +0500

    updated main js

--- ToDoList/js/main.js
+++ ToDoList/js/main.js
@@ -2,40 +2,48 @@ const addForm = document.getElementById('add-form');
 const inputTodo = document.getElementById('input-todo');
 const todoList = document.getElementById('todo-list');
 
-function addItem() {
-    const newItem = document.createElement('li');
-    newItem.setAttribute('class', 'todo-item');
-    newItem.textContent = inputTodo.value;
+const oldTodos = JSON.parse(localStorage.getItem('todos'));
+let activeTodos = Array();
+// if (oldTodos) {
+//     activeTodos = [...oldTodos]
+// }
 
-    todoList.appendChild(newItem);
-    inputTodo.value = "";
-    inputTodo.focus();
-}
+// console.log(activeTodos);
+// for (const todo of activeTodos) {
+//     addItemToList(todo);
+// }
 
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    if (inputTodo.value != "") {
-        addItem();
+    const itemText = inputTodo.value;
+    if (itemText) {
+        addItemToList(itemText);
+        addItemToStorage(itemText);
     }
     inputTodo.focus();
 });
 
-todoList.addEventListener('click', (e) => {
-    const item = e.target;
-    if (item.classList[1] === 'done') {
-        item.classList.remove('done');
-    } else {
-        item.classList.add('done');
-    }
-    inputTodo.focus();
-});
 
-todoList.addEventListener('dblclick', (e) => {
-    const item = e.target;
-    todoList.removeChild(item);
-    inputTodo.focus();
-});
+// todoList.addEventListener('click', (e) => {
+//     const item = e.target;
+//     item.classList.toggle('done');
+//     inputTodo.focus();
+// });
+// todoList.addEventListener('dblclick', (e) => {
+//     const item = e.target;
+//     todoList.removeChild(item);
+//     inputTodo.focus();
+// });
 
 
+function addItemToList(itemText) {
+    const newItem = document.createElement('li');
+    newItem.classList.add('todo-item');
+    newItem.textContent = itemText;
+    todoList.appendChild(newItem);
+}
 
+function addItemToStorage(itemText) {
+    activeTodos.push(itemText);
+    localStorage.setItem('todos', JSON.stringify(activeTodos));
+}
